8607fb930cde70b861eff7ddec5a1589b3be058f
Anjana Jolly <anjana.j@cumulations.com>
2024-02-19 16:58:04 +0530

    new Updates

--- webcontents/settings.py
+++ webcontents/settings.py
@@ -134,10 +134,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = '/usr/local/lsws/Cashew_corp/env/public/static/'
+STATIC_ROOT = '/usr/local/lsws/kscdc_vh/html/public/static/'
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = '/usr/local/lsws/Cashew_corp/env/public/media/'
+MEDIA_ROOT = '/usr/local/lsws/kscdc_vh/html/public/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
